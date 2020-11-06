5b6018014a0b7d77863a8065e35fc3e235d319bc
KC Willard <willarke@oregonstate.edu>
2020-11-05 23:54:32 -0800

    First database call to retrieve ingredients from recipe

--- flaskr/routes.py
+++ flaskr/routes.py
@@ -33,11 +33,12 @@ def search_for_recipe():
     result = execute_query(id_query)
 #   Convert result tuple to integer
     recipe_id = result[0][0]    
-    query = "SELECT ingredients.name FROM ingredients INNER JOIN recipes_ingredients ON ingredients.id =\
-    recipes_ingredients.ingredient_id WHERE recipes_ingredients.recipe_id = %d;" %(recipe_id)
+    query = "SELECT ingredients.name, ingredients.description, ingredients.origin FROM ingredients\
+    INNER JOIN recipes_ingredients ON ingredients.id = recipes_ingredients.ingredient_id\
+    WHERE recipes_ingredients.recipe_id = %d;" %(recipe_id)
 #   Convert result tuple to list and then just get the first element of the tuple      
-    result = list(execute_query(query))    
-    ingredient_list =[item for t in result for item in t]
+    ingredient_list = list(execute_query(query))        
+#    ingredient_list =[item for t in result for item in t]
 #   Pass the search query and the list of ingredients to the new html for display.
     return render_template('recipe_display.html', name=recipe_name, ingredients=ingredient_list)
 
