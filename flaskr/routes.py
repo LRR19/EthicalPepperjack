32fd13413f4e7553a1a2c99b4709ac80c44cb5c8
Pavel Gordeyev <gordeyev@Pavels-MacBook-Pro-2.local>
2020-11-05 19:14:56 -0800

    merging into master

--- flaskr/routes.py
+++ flaskr/routes.py
@@ -1,25 +1,25 @@
-from flaskr import app, db_connect
+from flaskr import app
 from flask import render_template, request, redirect, jsonify, url_for, flash, session
 
 
 @app.route('/')
 def index():
 
-	# return render_template('file', title='')
+	return render_template('base.html', title='test')
 
-@app.route('/home', methods=['GET','POST'])
-def home():
+# @app.route('/home', methods=['GET','POST'])
+# def home():
 
-	# Sample query
-	# query = """SELECT * FROM table JOIN table.id = table2.id WHERE table.id = %d;""" %(table_id)
+# 	# Sample query
+# 	# query = """SELECT * FROM table JOIN table.id = table2.id WHERE table.id = %d;""" %(table_id)
 
-	# Execute query
-	# results = db_connect.execute_query(query)
+# 	# Execute query
+# 	# results = db_connect.execute_query(query)
 
-@app.errorhandler(404)
-def pageNotFound(error):
-	return render_template('404.html', title='Page Not Found')
+# @app.errorhandler(404)
+# def pageNotFound(error):
+# 	return render_template('404.html', title='Page Not Found')
 
-@app.errorhandler(500)
-def majorError(error):
-	return render_template('500.html', title='Major Error')
+# @app.errorhandler(500)
+# def majorError(error):
+# 	return render_template('500.html', title='Major Error')
