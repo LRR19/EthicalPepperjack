72e775fd3c1285e545da7507eb4a8dd6fada028e
leyden recinos <leyden.recinos@gmail.com>
2020-11-07 16:14:02 -0800

    Added a post/get route to route.py

--- flaskr/routes.py
+++ flaskr/routes.py
@@ -1,63 +1,81 @@
 from flaskr import app
-from flask import render_template, request, redirect, jsonify, url_for, flash, session
+from flask import render_template, request, redirect, jsonify, url_for, flash, \
+    session
 from flask_mysqldb import MySQL
 from .db_connect import *
-
 
 mysql = MySQL(app)
 
 
 @app.route('/')
 def index():
-
     return render_template('base.html', title='test')
 
 
-@app.route('/search_category')
+@app.route('/search_category', methods=['GET', 'POST'])
 def search_category():
+    if request.method == 'GET':
+        return render_template('search_category.html')
+    elif request.method == 'POST':
+        user_data = request.form
+        ethical_category = user_data['search_category']
+        ingredient_list = []
 
-    return render_template('search_category.html')
+        select_query = """SELECT * FROM ethical_categories WHERE name = %s"""
+
+        data = execute_query(select_query, (ethical_category))
+
+        ethical_concern = """SELECT ethical_concerns.name FROM 
+        ethical_concerns INNER JOIN ingredients_concerns ON 
+        ethical_concerns.id = ingredients_concerns.concern_id  WHERE 
+        ingredient_concerns.ingredient_id = %s"""
+        data2 = execute_query(ethical_concern, str(data[0][0]))
+        return render_template('search_category.html', name=ethical_category,
+                               ingredients=ingredient_list)
 
 
 
-##This will be all of the routes for the recipe book function. There will be a Get to get the current users recipe
-##book. Delete will remove a recipe.
+
+
+# This will be all of the routes for the recipe book function. There will be a
+# Get to get the current users recipe
+# book. Delete will remove a recipe.
 
 @app.route('/recipebook')
 def recipebook():
-
     return render_template('recipe_book/user.html')
+
 
 # Route to handle the display of ingredients after searching for a recipe.
 # Recipe name is the input and will return list of all ingredients
-@app.route('/recipesearch')
+@app.route('/recipesearch', methods=['GET'])
 def search_for_recipe():
-#   Get the recipe name  from the search bar
-#   recipe_name = request.args.get("recipe_name")
-    recipe_name = "tomato soup"#   
-#   Find the associated recipe ID with the recipe name
-    id_query = "SELECT id FROM recipes WHERE name =\'%s\';" %(recipe_name)    
+    #   Get the recipe name  from the search bar
+    #   recipe_name = request.args.get("recipe_name")
+    recipe_name = "tomato soup"  #
+    #   Find the associated recipe ID with the recipe name
+    id_query = "SELECT id FROM recipes WHERE name =\'%s\';" % recipe_name
     result = execute_query(id_query)
-#   Convert result tuple to integer
-    recipe_id = result[0][0]    
+    #   Convert result tuple to integer
+    recipe_id = result[0][0]
     query = "SELECT ingredients.name, ingredients.description, ingredients.origin FROM ingredients\
     INNER JOIN recipes_ingredients ON ingredients.id = recipes_ingredients.ingredient_id\
-    WHERE recipes_ingredients.recipe_id = %d;" %(recipe_id)
-#   Convert result tuple to list and then just get the first element of the tuple      
-    ingredient_list = list(execute_query(query))        
-#    ingredient_list =[item for t in result for item in t]
-#   Pass the search query and the list of ingredients to the new html for display.
-    return render_template('recipe_display.html', name=recipe_name, ingredients=ingredient_list)
-
-
-
-    
+    WHERE recipes_ingredients.recipe_id = %d;" % (recipe_id)
+    #   Convert result tuple to list and then just get the first element of
+    #   the tuple
+    ingredient_list = list(execute_query(query))
+    #    ingredient_list =[item for t in result for item in t]
+    #   Pass the search query and the list of ingredients to the new html for
+    #   display.
+    return render_template('recipe_display.html', name=recipe_name,
+                           ingredients=ingredient_list)
 
 # @app.route('/home', methods=['GET','POST'])
 # def home():
 
 # 	# Sample query
-# 	# query = """SELECT * FROM table JOIN table.id = table2.id WHERE table.id = %d;""" %(table_id)
+# 	# query = """SELECT * FROM table JOIN table.id = table2.id WHERE
+# 	table.id = %d;""" %(table_id)
 
 # 	# Execute query
 # 	# results = db_connect.execute_query(query)
