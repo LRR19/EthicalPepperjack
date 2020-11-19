c71eb80f696f7ea48530c3ae8cad603c5e23f4c2
Anthony Trinh <trinhan@oregonstate.edu>
2020-11-19 13:50:59 -0800

    Separated out user class
    -Moved initialization of loginmanager to the init file
    -User class now within models.py
    -Profile uses only the user_obj variables

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from config import Config
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object(Config)
 app.secret_key = 'test key for flasklogin'
-
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 
 from flaskr import routes
